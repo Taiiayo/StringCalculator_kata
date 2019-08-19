97feffc08c0c14bf44fefe364385c603539db158
taiiya <taiiakinyo@gmail.com>
2019-08-19 16:52:00 +0300

    first two cases done

---
+++ StringCalc_kata/UnitTest1.cs
@@ -0,0 +1,52 @@
+using System;
+using Xunit;
+using StringCalculator_kata;
+
+namespace StringCalc_kata
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Add()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+
+            var actualResult = stringCalculator.Add("");
+
+            Assert.Equal(0, actualResult);
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("8")]
+        [InlineData("10")]
+        public void AddOne(string numbers)
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+
+            var actualResult = stringCalculator.Add(numbers);
+
+            Assert.Equal(int.Parse(numbers), actualResult);
+        }
+
+        [Fact]
+        public void AddTwo()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+
+            var actualResult = stringCalculator.Add("22,13");
+
+            Assert.Equal(35, actualResult);
+        }
+
+        [Fact]
+        public void AddUnknown()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+
+            var actualResult = stringCalculator.Add("13,2,24,45");
+
+            Assert.Equal(84, actualResult);
+        }
+    }
+}
---
+++ StringCalculator_kata/StringCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator_kata
+{
+    public class StringCalculator
+    {
+        private const char delimiter = ',';
+
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            var ints = numbers.Split(delimiter).ToList();
+            if (ints.Count == 1)
+            {
+                return int.Parse(numbers);
+            }
+            else if(ints.Count > 1)
+            {
+                var intArr = ints.Select(int.Parse).ToArray();
+                return intArr.Sum();
+            }
+
+
+            return 5;
+        }
+    }
+}
